a2149603508d142e1d7442b7996006459c465207
Axel <axel@lighton.ai>
2022-09-06 16:08:11 +0200

    Add hendrycks hotfix

--- lm_eval/base.py
+++ lm_eval/base.py
@@ -444,6 +444,7 @@ class Task(abc.ABC):
             data_dir=data_dir,
             cache_dir=cache_dir,
             download_mode=download_mode,
+            ignore_verifications=self.DATASET_PATH == "hendrycks_test"
         )
 
     def should_decontaminate(self):
